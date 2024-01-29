50e1da576f68f4daa4601edcb646d2a97733b16a
MiguelwLD <114840612+Miguel7293@users.noreply.github.com>
2024-01-29 00:14:15 -0500

    ConFe
    ConFe

---
+++ sistemas-app/resources/js/Components/common/CollectionSelector.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { Ref } from 'vue';
+defineProps({
+    collection:{
+        type: Array,
+        required: true
+    }
+})
+
+const currentSelection = ref(1)
+const selection = ref([])
+
+const handAddToSelection =()=>{
+    selection.value.push(currentSelection)
+}
+</script>
+
+<template>
+    <select v-model="currentSelection">
+        <option v-for="(item, index) in collection" :key="index"> {{ item?.name }}</option>
+    </select>
+    <button @click="handAddToSelection">Add</button>
+    <div>
+        <ul>
+            <li v-for="(item, index) in selection" :key="index">
+                {{ item}}
+            </li>
+        </ul>
+    </div>
+</template>
